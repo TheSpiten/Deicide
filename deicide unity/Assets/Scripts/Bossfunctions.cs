945c5a0dea50fffe1898d36fabe0c6fd26fc95ad
Jesper Pettersson <TheSpiten@users.noreply.github.com>
2019-02-08 14:42:18 +0100

    Collision script
    Need to fix bullet tags so they can collide with boss, Or othe way around.

--- deicide unity/Assets/Scripts/Bossfunctions.cs
+++ deicide unity/Assets/Scripts/Bossfunctions.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Bossfunctions : MonoBehaviour
 {
+        private int counter;
+
+        public List<Color> colors; //list of colors, can be changed in inspector
+
+        SpriteRenderer renderReference;
+
+        void OnCollisionEnter2D(Collision2D coll)
+        {
+            if (coll.gameObject.tag == "Bullet") { //uncomment to check tag of colliding object
+                if (counter < colors.Count - 1)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 0;
+                }
+                renderReference.color = colors[counter];
+            } //uncomment to check tag of colliding object
+        }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = 0;
+        renderReference = GetComponent<SpriteRenderer>();
+        renderReference.color = colors[counter];
+
     }
 
     // Update is called once per frame
