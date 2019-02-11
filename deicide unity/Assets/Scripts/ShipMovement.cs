ae7eb5cd5d42c95d484d26aa98d9c7c69c8a033d
danielmichaneck5180 <Daniel.Michaneck.5180@student.uu.se>
2019-02-11 16:47:20 +0100

    Added dash functionality
    Added the dash function and a change to input handling in ShipMovement script

--- deicide unity/Assets/Scripts/ShipMovement.cs
+++ deicide unity/Assets/Scripts/ShipMovement.cs
@@ -9,23 +9,91 @@ public class ShipMovement : MonoBehaviour
     public GameObject Bullet;
     Rigidbody2D rb;
     public float speed;
+    public float dashDelay;
+    public float dashMultiplier;
+    public float dragMultiplier;
+    public float dashSpeedIncrease;
+    public float dashSpeedDuration;
     int health = 3;
+    private float dashTimer;
+    private float dashSpeedTimer;
+    private float dashCurrentIncreasedSpeed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         Gun = transform.Find("Gun").gameObject;
+        dashCurrentIncreasedSpeed = 1;
     }
 
+    // Should maybe be FixedUpdate()?
     void Update()
     {
+        // Updates dash speed
+        float newVelocityX = rb.velocity.x;
+        float newVelocityY = rb.velocity.y;
+        int signVelocityX = Mathf.FloorToInt(Mathf.Sign(rb.velocity.x));
+        int signVelocityY = Mathf.FloorToInt(Mathf.Sign(rb.velocity.y));
+        if (Mathf.Abs(rb.velocity.x) > speed)
+        {
+            newVelocityX = rb.velocity.x - (rb.drag * dragMultiplier * Mathf.Sign(rb.velocity.x));
+            if (Mathf.Sign(newVelocityX) != signVelocityX)
+            {
+                newVelocityX = 0;
+            }
+        }
+        if (Mathf.Abs(rb.velocity.y) > speed)
+        {
+            newVelocityY = rb.velocity.y - (rb.drag * dragMultiplier * Mathf.Sign(rb.velocity.y));
+            if (Mathf.Sign(newVelocityY) != signVelocityY)
+            {
+                newVelocityY = 0;
+            }
+        }
+        rb.velocity = new Vector2(newVelocityX, newVelocityY);
+
+        // Updates dashTimer
+        dashTimer = TimerTick(dashTimer);
+        // Updates dashSpeedTimer
+        dashSpeedTimer = TimerTick(dashSpeedTimer);
+
         // Getting input for movement
-        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0));
-        rb.AddForce(new Vector2(0, Input.GetAxisRaw("Vertical") * speed));
+        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed * dashCurrentIncreasedSpeed, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxisRaw("Vertical") * speed * dashCurrentIncreasedSpeed));
+        
+        // Declares input keys
+        var shootKey = KeyCode.Z;
+        var dashKey = KeyCode.X;
+        var powerupKey = KeyCode.C;
 
         // Shooting on left click. Modify number after "delay >" to change the time between shots
-        if (Input.GetMouseButton(0) && delay > 20)
-            Shoot();
+        // Also shoots if shootKey is pressed
+        if (Input.GetMouseButton(0) || Input.GetKey(shootKey))
+        {
+            if (delay > 20)
+            {
+                Shoot();
+            }
+        }
+        
+        // Dashes if dashKey (or right click?) is pressed
+        if (Input.GetMouseButton(1) || Input.GetKeyDown(dashKey))
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                if (dashTimer == 0)
+                {
+                    Dash();
+                    dashTimer = dashDelay;
+                }
+            }
+        }
+
+        // Resets speed increase caused by dashing
+        if (dashSpeedTimer == 0)
+        {
+            dashCurrentIncreasedSpeed = 1;
+        }
 
         delay++;
     }
@@ -42,7 +110,16 @@ public class ShipMovement : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * 15;
         Destroy(bullet, 2.0f);
         delay = 0;
+        // Plays shooting sound
         GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().PlaySound(1);
+    }
+
+    private void Dash()
+    {
+        rb.velocity += new Vector2(Input.GetAxisRaw("Horizontal") * speed * dashMultiplier, Input.GetAxisRaw("Vertical") * speed * dashMultiplier);
+
+        dashCurrentIncreasedSpeed = dashSpeedIncrease;
+        dashSpeedTimer = dashSpeedDuration;
     }
 
     // Ship takes 1 damage
@@ -51,5 +128,20 @@ public class ShipMovement : MonoBehaviour
         health--;
         if (health == 0)
             Destroy(gameObject);
+    }
+
+    private float TimerTick(float currentTime)
+    {
+        float returnTime = currentTime;
+        // Updates timer
+        if (currentTime > 0)
+        {
+            returnTime -= Time.deltaTime;
+        }
+        if (currentTime < 0)
+        {
+            returnTime = 0;
+        }
+        return returnTime;
     }
 }
