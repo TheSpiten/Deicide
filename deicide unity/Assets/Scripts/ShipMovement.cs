0887f67801df0a61741a3afdd03062fefdc686b6
danielmichaneck5180 <Daniel.Michaneck.5180@student.uu.se>
2019-02-13 14:11:38 +0100

    Revert "Update from master"
    This reverts commit 232520220fd75239e610fcf01ba5e0ddaaa76480, reversing
    changes made to b06e777ddd291b2b89ffa87c4750ba63929a650c.

--- deicide unity/Assets/Scripts/ShipMovement.cs
+++ deicide unity/Assets/Scripts/ShipMovement.cs
@@ -9,95 +9,23 @@ public class ShipMovement : MonoBehaviour
     public GameObject Bullet;
     Rigidbody2D rb;
     public float speed;
-    public float dashDelay;
-    public float dashMultiplier;
-    public float dragMultiplier;
-    public float dashSpeedIncrease;
-    public float dashSpeedDuration;
     int health = 3;
-    private float dashTimer;
-    private float dashSpeedTimer;
-    private float dashCurrentIncreasedSpeed;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         Gun = transform.Find("Gun").gameObject;
-        dashCurrentIncreasedSpeed = 1;
     }
 
-    // Should maybe be FixedUpdate()?
-    void FixedUpdate()
+    void Update()
     {
-        // Updates dash speed
-        float newVelocityX = rb.velocity.x;
-        float newVelocityY = rb.velocity.y;
-        int signVelocityX = Mathf.FloorToInt(Mathf.Sign(rb.velocity.x));
-        int signVelocityY = Mathf.FloorToInt(Mathf.Sign(rb.velocity.y));
-        if (Mathf.Abs(rb.velocity.x) > speed)
-        {
-            newVelocityX = rb.velocity.x - (rb.drag * dragMultiplier * Mathf.Sign(rb.velocity.x));
-            if (Mathf.Sign(newVelocityX) != signVelocityX)
-            {
-                newVelocityX = 0;
-            }
-        }
-        if (Mathf.Abs(rb.velocity.y) > speed)
-        {
-            newVelocityY = rb.velocity.y - (rb.drag * dragMultiplier * Mathf.Sign(rb.velocity.y));
-            if (Mathf.Sign(newVelocityY) != signVelocityY)
-            {
-                newVelocityY = 0;
-            }
-        }
-        rb.velocity = new Vector2(newVelocityX, newVelocityY);
-
         // Getting input for movement
-        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed * dashCurrentIncreasedSpeed, 0));
-        rb.AddForce(new Vector2(0, Input.GetAxisRaw("Vertical") * speed * dashCurrentIncreasedSpeed));
-        
-    }
-
-    private void Update()
-    {
-        // Updates dashTimer
-        dashTimer = TimerTick(dashTimer);
-        // Updates dashSpeedTimer
-        dashSpeedTimer = TimerTick(dashSpeedTimer);
-
-        // Declares input keys
-        var shootKey = KeyCode.Z;
-        var dashKey = KeyCode.X;
-        var powerupKey = KeyCode.C;
+        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxisRaw("Vertical") * speed));
 
         // Shooting on left click. Modify number after "delay >" to change the time between shots
-        // Also shoots if shootKey is pressed
-        if (Input.GetMouseButton(0) || Input.GetKey(shootKey))
-        {
-            if (delay > 20)
-            {
-                Shoot();
-            }
-        }
-
-        // Dashes if dashKey (or right click?) is pressed
-        if (Input.GetMouseButton(1) || Input.GetKeyDown(dashKey))
-        {
-            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-            {
-                if (dashTimer == 0)
-                {
-                    Dash();
-                    dashTimer = dashDelay;
-                }
-            }
-        }
-
-        // Resets speed increase caused by dashing
-        if (dashSpeedTimer == 0)
-        {
-            dashCurrentIncreasedSpeed = 1;
-        }
+        if (Input.GetMouseButton(0) && delay > 20)
+            Shoot();
 
         delay++;
     }
@@ -114,16 +42,6 @@ public class ShipMovement : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * 15;
         Destroy(bullet, 2.0f);
         delay = 0;
-        // Plays shooting sound
-        GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().PlaySound(1);
-    }
-
-    private void Dash()
-    {
-        rb.velocity += new Vector2(Input.GetAxisRaw("Horizontal") * speed * dashMultiplier, Input.GetAxisRaw("Vertical") * speed * dashMultiplier);
-
-        dashCurrentIncreasedSpeed = dashSpeedIncrease;
-        dashSpeedTimer = dashSpeedDuration;
     }
 
     // Ship takes 1 damage
@@ -132,20 +50,5 @@ public class ShipMovement : MonoBehaviour
         health--;
         if (health == 0)
             Destroy(gameObject);
-    }
-
-    private float TimerTick(float currentTime)
-    {
-        float returnTime = currentTime;
-        // Updates timer
-        if (currentTime > 0)
-        {
-            returnTime -= Time.deltaTime;
-        }
-        if (currentTime < 0)
-        {
-            returnTime = 0;
-        }
-        return returnTime;
     }
 }
