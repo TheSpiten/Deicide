b06e777ddd291b2b89ffa87c4750ba63929a650c
Axel Thomsen <axelthomsen@hotmail.com>
2019-02-13 14:02:18 +0100

    Background Stuff
    Background Stuff

---
+++ deicide unity/Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPosition + Vector3.right * newPosition;
+    }
+}
