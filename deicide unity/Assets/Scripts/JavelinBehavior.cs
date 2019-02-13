0cebeb7412c9bd9abeefab3eb8f9ca76bdbc03e3
Jesper Pettersson <TheSpiten@users.noreply.github.com>
2019-02-13 14:59:20 +0100

    Javelin Done
    Javlin done now with place holder sound for when player gets hit

---
+++ deicide unity/Assets/Scripts/JavelinBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JavelinBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+
+        if (collision.gameObject.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<Bossfunctions>().HitBoss(collision.gameObject.tag);
+            //Destroy(collision);
+            GameObject.FindGameObjectWithTag("AudioManager").GetComponent<AudioManager>().PlaySound(0);
+        }
+
+
+
+
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
