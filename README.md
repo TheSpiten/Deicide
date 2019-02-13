7fafe35a646091bfc73544dc066f6ceeec355cac
danielmichaneck5180 <Daniel.Michaneck.5180@student.uu.se>
2019-02-13 14:36:04 +0100

    Revert "Goodbye cruel world"
    This reverts commit 0fa085e64b21837f5cd52fc58c4ab57d9787072f.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Deicide
+Group Project with Group Orcus
---
+++ deicide unity/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    int delay = 0;
+    public GameObject Gun;
+    public GameObject Bullet;
+    Rigidbody2D rb;
+    public float speed;
+    int health = 3;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Gun = transform.Find("Gun").gameObject;
+    }
+
+    void Update()
+    {
+        // Getting input for movement
+        rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * speed, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxisRaw("Vertical") * speed));
+
+        // Shooting on left click. Modify number after "delay >" to change the time between shots
+        if (Input.GetMouseButton(0) && delay > 20)
+            Shoot();
+
+        delay++;
+    }
+
+    void Shoot()
+    {
+        // Checking for mouse position and making a Quaternion of it
+        var MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        MousePos.z = 0;
+        var aim = Quaternion.FromToRotation(Vector3.right, MousePos - transform.position);
+
+        // Spawning bullet and shooting it towards the mouse
+        var bullet = (GameObject)Instantiate(Bullet, Gun.transform.position, aim);
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * 15;
+        Destroy(bullet, 2.0f);
+        delay = 0;
+    }
+
+    // Ship takes 1 damage
+    public void Damage()
+    {
+        health--;
+        if (health == 0)
+            Destroy(gameObject);
+    }
+}
