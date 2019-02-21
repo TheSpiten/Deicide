d27cffaba8b4fb7da4c43cf8c3b88f1058305b7d
Axel Thomsen <axelthomsen@hotmail.com>
2019-02-21 11:37:34 +0100

    Dynamite & Feathers (basic)
    Dynamite script/animation, feathers aimed at player

---
+++ deicide unity/Assets/BossAim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAim : MonoBehaviour
+{
+    void Update()
+    {
+        // Checking for ship position and aiming at it
+        var PlayerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        var aim = Quaternion.FromToRotation(Vector3.left, PlayerPos - transform.position);
+        transform.rotation = aim;
+    }
+}
