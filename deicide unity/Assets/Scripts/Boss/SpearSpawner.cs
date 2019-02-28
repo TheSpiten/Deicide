3ed57437c32967debf593aba06760821dfe1e41d
Jesper Pettersson <47108514+TheSpiten@users.noreply.github.com>
2019-02-28 16:40:19 +0100

    Spear Update
    Added a spawner that spawns spears when pressing "K"

---
+++ deicide unity/Assets/Scripts/Boss/SpearSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearSpawner : MonoBehaviour
+{
+    public GameObject Spear;
+    public float spearSpeed;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SpearInstantiate();
+        }
+
+        
+    }
+
+    public void SpearInstantiate()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            var spears = (GameObject)Instantiate(Spear, transform.position, transform.rotation);
+            spears.GetComponent<Rigidbody2D>().velocity = -spears.transform.up * spearSpeed;
+            Destroy(spears, 2.0f);
+        }
+    } 
+
+
+
+}
