ddab93afd4ee14b82a1e5c142a5daa07a9c98c6d
Jesper Pettersson <47108514+TheSpiten@users.noreply.github.com>
2019-03-18 03:03:14 +0100

    Spear "Done"
    Script added for the spears
    spear spawner script done but could use cleanup
    TODO: Add the actual art for spears and the animation for the boss before the attack

--- deicide unity/Assets/Scripts/Boss/SpearSpawner.cs
+++ deicide unity/Assets/Scripts/Boss/SpearSpawner.cs
@@ -7,14 +7,19 @@ public class SpearSpawner : MonoBehaviour
     public GameObject Spear;
     public float spearSpeed;
     public Vector2 SpawnPos;
+    private Transform spawnTransform;
+    // multi is the multiplier for the distance between the spears
+    public float multiX;
+    public float multiY;
     public float xPos;
+    public float yPos;
     
     
     
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -30,14 +35,19 @@ public class SpearSpawner : MonoBehaviour
 
     public void SpearInstantiate()
     {
-        for (int i = 0; i < 4; i++)
+        spawnTransform = GameObject.Find("SpearSpawner").transform;
+        for (int i = 1; i < 5; i++)
         {
             var spears = (GameObject)Instantiate(Spear, transform.position, transform.rotation);
             spears.GetComponent<Rigidbody2D>().velocity = -spears.transform.up * spearSpeed;
-            xPos = xPos + 1.0f;
-            transform.position = Vector2.right * xPos;
+            SpawnPos = new Vector2((xPos + (i * multiX)), (yPos + (i * multiY)));
+            spawnTransform.position = SpawnPos;
             Destroy(spears, 2.0f);
         }
+        xPos = -8.13293f;
+        yPos = 6.004479f;
+        SpawnPos = new Vector2((xPos), (yPos));
+        spawnTransform.position = SpawnPos;
     } 
 
 
---
+++ deicide unity/Assets/Scripts/Projectiles/Spear.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            // ShipMovement is a script name, careful with the names if you change/reuse
+            collision.gameObject.GetComponent<ShipMovement>().Damage(20);
+            Destroy(gameObject);
+        }
+    }
+}
